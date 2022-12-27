92a77df7f3c5346ab9a33ed29f6bece821d326e1
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-12-27 23:58:51 +0600

    index.js and .env file connected

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.le2w9sh.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const postDataCollection = client.db('socialMedia').collection('postData');
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Social-Media project is running ')
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Social Media project running on ${port}`)
+})
