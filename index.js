9bb853538da5ae1a055469376c6695fb5ae6f26e
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-12-30 23:46:43 +0600

    get api form id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -19,10 +19,42 @@ async function run() {
     try {
         const postDataCollection = client.db('socialMedia').collection('postData');
 
+        // insert post data
         app.post("/postData", async (req, res) => {
             const userPostData = req.body;
             const result = await postDataCollection.insertOne(userPostData);
+
             // console.log(result)
+            res.send(result);
+        })
+
+        // get post data
+        app.get("/postData", async (req, res) => {
+            const query = {};
+            const postdatas = postDataCollection.find(query).sort({ postTime: -1 });
+            const result = await postdatas.toArray();
+            res.send(result);
+        })
+
+        // get data by id
+        app.get("/postData/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const postData = await postDataCollection.findOne(query)
+            res.send(postData);
+        })
+
+        // update post data with like 
+        app.put("postData/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    likes: likeCounter
+                },
+            };
+            const result = await postDataCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         })
 
